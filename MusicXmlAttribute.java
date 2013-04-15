d0853c5f11712570d94936772b1b46d503eed2f7
Erick Daniel Corona <edcoronag@gmail.com>
2013-04-14 20:41:50 -0500

    initial upload. MusicXML Hello World! nodes can be printed out

---
+++ MusicXmlAttribute.java
@@ -0,0 +1,27 @@
+public class MusicXmlAttribute {
+  public MusicXmlAttribute() {
+    m_name = new String();
+    m_value = new String();
+  }
+  public MusicXmlAttribute(String name, String value) {
+    m_name = name;
+    m_value = value;
+  }
+
+  public void setName(String name) {
+    m_name = name;
+  }
+  public void setValue(String value) {
+    m_value = value;
+  }
+
+  public String getName() {
+    return m_name;
+  }
+  public String getValue() {
+    return m_value;
+  }
+  
+  private String m_name;
+  private String m_value;
+}
