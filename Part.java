c24a6af1be8f1b3359a72bf049e4e649a1e4ce9b
Erick Daniel Corona <edcoronag@gmail.com>
2013-04-20 22:27:11 -0500

    created class Part and removed license messages from individual files

---
+++ Part.java
@@ -0,0 +1,14 @@
+public class Part {
+  private LinkedList<Note> m_notes;
+  private String           m_name;
+  private String           m_id;
+  private String           m_instrument;
+
+  public MusicXmlNode getMusicXmlNode(){
+  }
+  public LinkedList<Note> getNotes() {
+  }
+  public void buildMusicXmlNode() {
+  }
+}
+
