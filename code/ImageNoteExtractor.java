9d0c90db32ee49c7564ca809d8354667a29f9f05
Erick Daniel Corona <edcoronag@gmail.com>
2013-05-10 22:37:33 -0500

    defined skeleton of the class

--- code/ImageNoteExtractor.java
+++ code/ImageNoteExtractor.java
@@ -2,39 +2,47 @@
 import javax.imageio.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 
 public class ImageNoteExtractor {
-  private static BufferedImage m_image;
+  public static final int DETECT_MODE_VERTICAL =   0;
+  public static final int DETECT_MODE_HORIZONTAL = 1;
+  public static final int DETECT_MODE_DIAGONAL =   2;
   
+  public static final int DETECT_TRESHOLD_DEFAULT =         3;
+  public static final int VERTICAL_RESOLUTION_DEFAULT =    10;
+  public static final int HORIZONTAL_RESOULUTION_DEFAULT = 10;
+
+
+  private BufferedImage  m_image;
+  private int            m_detectMode;
+  private int            m_detectionTreshold;
+  private int            m_verticalResolution;
+  private int            m_horizontalResolution;
+  private String         m_fileName;
+  private ArrayList<Integer> m_edges;
+
+
   public ImageNoteExtractor() {
-    
+    m_image = null;
+    m_detectMode = DETECT_MODE_VERTICAL;
+    m_detectionTreshold = DETECT_TRESHOLD_DEFAULT;
+    m_verticalResolution = VERTICAL_RESOLUTION_DEFAULT;
+    m_horizontalResolution = HORIZONTAL_RESOULUTION_DEFAULT;
+    m_fileName = null;
   }
   public ImageNoteExtractor(String fileName) {
+    m_detectMode = DETECT_MODE_VERTICAL;
+    m_detectionTreshold = DETECT_TRESHOLD_DEFAULT;
+    m_verticalResolution = VERTICAL_RESOLUTION_DEFAULT;
+    m_horizontalResolution = HORIZONTAL_RESOULUTION_DEFAULT;
+    m_fileName = fileName;
     try {
-      m_image = ImageIO.read(new File("strawberry.jpg"));
+      m_image = ImageIO.read(new File(m_fileName));
     }
     catch (IOException ioException) {
       System.err.println("ImageNoteExtractor: error opening image");
     }
-  }
-  
-  // TODO: remove main method
-  public static void main(String[] args) {
-    try {
-      m_image = ImageIO.read(new File("../tests/strawberry.jpg"));
-    }
-    catch (IOException ioException) {
-      System.err.println("ImageNoteExtractor: error opening image");
-    }
-    System.out.println("image height: " + m_image.getHeight());
-    System.out.println("image width: " + m_image.getWidth());
-    for (int i = 0; i < m_image.getHeight() / 2; ++i) {
-      for (int j = 0; j < m_image.getWidth() / 2; ++j) {
-        System.out.println("pixel at: [" + i +", " + j + "]: " + 
-                           m_image.getRGB(i, j));
-      }
-    }
-    System.out.println("ToString: " + m_image.toString());
   }
 }
