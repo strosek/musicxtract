ff4ce52aaed3b3dd2d9ed5ecbc46b681d05f62b1
Erick Daniel Corona <edcoronag@gmail.com>
2013-05-11 21:28:12 -0500

    added setters and getters to class ImageNoteExtractor

--- code/ImageNoteExtractor.java
+++ code/ImageNoteExtractor.java
@@ -2,7 +2,7 @@
 import javax.imageio.*;
 import java.io.*;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 
 public class ImageNoteExtractor {
@@ -15,13 +15,12 @@ public class ImageNoteExtractor {
   public static final int HORIZONTAL_RESOULUTION_DEFAULT = 10;
 
 
-  private BufferedImage  m_image;
-  private int            m_detectMode;
-  private int            m_detectionTreshold;
-  private int            m_verticalResolution;
-  private int            m_horizontalResolution;
-  private String         m_fileName;
-  private ArrayList<Integer> m_edges;
+  private BufferedImage       m_image;
+  private int                 m_detectMode;
+  private int                 m_detectionTreshold;
+  private int                 m_verticalResolution;
+  private int                 m_horizontalResolution;
+  private LinkedList<Integer> m_edges;
 
 
   public ImageNoteExtractor() {
@@ -30,19 +29,57 @@ public class ImageNoteExtractor {
     m_detectionTreshold = DETECT_TRESHOLD_DEFAULT;
     m_verticalResolution = VERTICAL_RESOLUTION_DEFAULT;
     m_horizontalResolution = HORIZONTAL_RESOULUTION_DEFAULT;
-    m_fileName = null;
   }
   public ImageNoteExtractor(String fileName) {
     m_detectMode = DETECT_MODE_VERTICAL;
     m_detectionTreshold = DETECT_TRESHOLD_DEFAULT;
     m_verticalResolution = VERTICAL_RESOLUTION_DEFAULT;
     m_horizontalResolution = HORIZONTAL_RESOULUTION_DEFAULT;
-    m_fileName = fileName;
     try {
-      m_image = ImageIO.read(new File(m_fileName));
+      m_image = ImageIO.read(new File(fileName));
     }
     catch (IOException ioException) {
       System.err.println("ImageNoteExtractor: error opening image");
     }
+  }
+  
+  public void setImage(BufferedImage image) {
+    m_image = image;
+  }
+  public void setDetectionMode(int mode) {
+    m_detectMode = mode;
+  }
+  public void setDetectionTreshold(int treshold) {
+    m_detectionTreshold = treshold;
+  }
+  public void setVerticalResolution(int resolution) {
+    m_verticalResolution = resolution;
+  }
+  public void setHorizontalResolution(int resolution) {
+    m_horizontalResolution = resolution;
+  }
+  public void openImageFile(String fileName) {
+    try {
+      m_image = ImageIO.read(new File(fileName));
+    }
+    catch (IOException ioException) {
+      System.err.println("ImageNoteExtractor: error opening image");
+    }
+  }
+
+  public int getDetectionMode() {
+    return m_detectMode;
+  }
+  public int getDetectionTreshold() {
+    return m_detectionTreshold;
+  }
+  public int getVerticalResolution() {
+    return m_verticalResolution;
+  }
+  public int getHorizontalResolution() {
+    return m_horizontalResolution;
+  }
+  public LinkedList<Integer> getEdges() {
+    return m_edges;
   }
 }
