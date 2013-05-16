965dd9f73fd098165a4bd3f29ebaccc4ba33d3cc
Erick Daniel Corona <edcoronag@gmail.com>
2013-05-15 21:48:19 -0500

    changed parameters for new constructor of Note

--- code/TestNote.java
+++ code/TestNote.java
@@ -3,10 +3,10 @@ public class TestNote {
     Note note = new Note();
     System.out.print(note.getMusicXmlNode().getXmlString(0));
 
-    Note note2 = new Note(Note.PITCH_G5S, 1, 4, "eigth", true, false);
+    Note note2 = new Note(Note.PITCH_G5S, 1, 4, "eigth", true, false, Note.TIE_NONE);
     System.out.print(note2.getMusicXmlNode().getXmlString(0));
     
-    Note note3 = new Note(true, 4, "eigth", true);
+    Note note3 = new Note(true, 4, "eigth", true, Note.TIE_START);
     System.out.print(note3.getMusicXmlNode().getXmlString(0));
   }
 }
