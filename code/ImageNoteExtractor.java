5c9be91a79ee30656259b63bff0bbf9683d22342
Erick Daniel Corona <edcoronag@gmail.com>
2013-05-21 21:22:31 -0500

    Implemented ImageNoteExtractor with android classes

--- code/ImageNoteExtractor.java
+++ code/ImageNoteExtractor.java
@@ -1,51 +1,38 @@
-// FIXME make import more specific
-import javax.imageio.*;
-import java.io.*;
-import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 
 
 public class ImageNoteExtractor {
   public static final int DETECT_MODE_VERTICAL =   0;
   public static final int DETECT_MODE_HORIZONTAL = 1;
-  public static final int DETECT_MODE_DIAGONAL =   2;
   
-  public static final int DETECT_TRESHOLD_DEFAULT =         3;
-  public static final int VERTICAL_RESOLUTION_DEFAULT =    10;
-  public static final int HORIZONTAL_RESOULUTION_DEFAULT = 10;
+public static final int DETECT_TRESHOLD_DEFAULT = 100;
 
+  public static final int VERTICAL_RESOLUTION_DEFAULT =    640;
+  public static final int HORIZONTAL_RESOULUTION_DEFAULT = 380;
+  
+  public static final int SAMPLES_DEFAULT = 16;
+  
 
-  private BufferedImage       m_image;
   private int                 m_detectMode;
   private int                 m_detectionTreshold;
   private int                 m_verticalResolution;
   private int                 m_horizontalResolution;
+  private int                 m_samples;
   private LinkedList<Integer> m_edges;
+  private NoteFactory         m_noteFactory;
 
 
   public ImageNoteExtractor() {
-    m_image = null;
     m_detectMode = DETECT_MODE_VERTICAL;
     m_detectionTreshold = DETECT_TRESHOLD_DEFAULT;
     m_verticalResolution = VERTICAL_RESOLUTION_DEFAULT;
     m_horizontalResolution = HORIZONTAL_RESOULUTION_DEFAULT;
+    m_samples = SAMPLES_DEFAULT;
+    m_edges = new LinkedList<Integer>();
+    m_noteFactory = new NoteFactory();
   }
-  public ImageNoteExtractor(String fileName) {
-    m_detectMode = DETECT_MODE_VERTICAL;
-    m_detectionTreshold = DETECT_TRESHOLD_DEFAULT;
-    m_verticalResolution = VERTICAL_RESOLUTION_DEFAULT;
-    m_horizontalResolution = HORIZONTAL_RESOULUTION_DEFAULT;
-    try {
-      m_image = ImageIO.read(new File(fileName));
-    }
-    catch (IOException ioException) {
-      System.err.println("ImageNoteExtractor: error opening image");
-    }
-  }
-  
-  public void setImage(BufferedImage image) {
-    m_image = image;
-  }
+
+
   public void setDetectionMode(int mode) {
     m_detectMode = mode;
   }
@@ -58,13 +45,8 @@ public class ImageNoteExtractor {
   public void setHorizontalResolution(int resolution) {
     m_horizontalResolution = resolution;
   }
-  public void openImageFile(String fileName) {
-    try {
-      m_image = ImageIO.read(new File(fileName));
-    }
-    catch (IOException ioException) {
-      System.err.println("ImageNoteExtractor: error opening image");
-    }
+  public void setSamples(int samples) {
+    m_samples = samples;
   }
 
   public int getDetectionMode() {
@@ -79,7 +61,59 @@ public class ImageNoteExtractor {
   public int getHorizontalResolution() {
     return m_horizontalResolution;
   }
-  public LinkedList<Integer> getEdges() {
-    return m_edges;
+  public int getSamples() {
+    return m_samples;
+  }
+  public NoteFactory getNoteFactory() {
+    return m_noteFactory;
+  }
+
+  public void computeEdges(Bitmap image) {
+    int maxDeltaLuminosity = m_detectionTreshold;
+    int position = -1;
+    int previousLuminosity, currentLuminosity, deltaLuminosity = 0;
+    Color color;
+    LinkedList<Integer> edges;
+    
+    int verticalLimit, horizontalLimit;
+    if (m_detectMode == DETECT_MODE_VERTICAL) {
+      horizontalLimit = image.getWidth();
+      verticalLimit = image.getHeight();
+    }
+    else {
+      horizontalLimit = image.getHeight();
+      verticalLimit = image.getWidth();
+    }
+    
+    for (int i = 0; i < horizontalLimit; ++i) {
+      for (int j = 0; j < verticalLimit; ++j) {
+        if (m_detectMode == DETECT_MODE_VERTICAL)
+          color = image.getPixel(i, j);
+        else
+          color = image.getPixel(j, i);
+        currentLuminosity = color.red() + color.green() + color.blue();
+        if (i > 0 || j > 0)
+          deltaLuminosity = Math.abs(previousLuminosity - currentLuminosity);
+        else
+          deltaLuminosity = 0;
+        if (deltaLuminosity > maxDeltaLuminosity) {
+          maxDeltaLuminosity = deltaLuminosity;
+          position = j;
+        }
+        previousLuminosity = currentLuminosity;
+      }
+      m_edges.add(position);
+    }
+  }
+  
+  public LinkedList<Note> getNotes(Bitmap image) {
+    computeEdges(image);
+    
+    LinkedList<Note> notes;
+    for (Integer edge : m_edges) {
+      notes.add(m_noteFactory.newNote(edge, Note.TYPE_QUARTER, false));
+    }
+    
+    return notes;
   }
 }
